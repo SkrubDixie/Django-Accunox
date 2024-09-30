d579720a9df2e8abda68712ac345497d17a5a246
Shubham <shubham.gupta9923.sa@gmail.com>
2024-09-30 22:10:43 +0530

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -1,21 +1,26 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello.settings")
-
-import django
-django.setup()
-
-from django.core.management import call_command
-
-from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db import models
 
-class Model1(models.Model):
+#Simple Class
+class My_Model(models.Model):
     name = models.CharField()
 
-@receiver(post_save, sender=Model1)
-def handler1(sender, instance, **kwargs):
-    print("Instance: "+ instance)
+# Defining signal receiver
+@receiver(post_save, sender=My_Model)
+def my_handler(sender, instance **kwargs):
+    print('Saved Instance: ' + instance.name )
 
-instance = Model1(name='Test')
-instance.save
+# Saving an instance
+
+instance = My_Model(name='Test Instance')
+instance.save()  # Triggering the post_save signal
+
+# The my_handler function is connected to the post_save signal of
+# the My_Model class which will be defined in models.py
+
+# When instance.save() will be called, the signal is sent
+# and my_handler is executed instantly with the output.
+
+# this demonstrates that signal handler execute in the same time
+# as signal sender, which shows synchronous behaviour.
